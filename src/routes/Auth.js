e7360320abe6ec6743e662c3a1483df01c1d193e
yuj1818 <95585314+yuj1818@users.noreply.github.com>
2022-11-11 18:00:26 +0900

    트위터 클론 코딩

---
+++ src/routes/Auth.js
@@ -0,0 +1,34 @@
+import React, {useState} from "react";
+
+const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const onChange = e => {
+        const {target: {name, value}} = e;
+        if(name === 'email'){
+            setEmail(value);
+        } else if(name === 'password'){
+            setPassword(value);
+        }
+    }
+    const onSubmit = e => {
+        e.preventDefault();
+
+    }
+    return(
+        <div>
+            <form onSubmit={onSubmit}>
+                <input name="email" type="email" placeholder="Email" required value={email} onChange={onChange}/>
+                <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange}/>
+                <input type="submit" value="LogIn" />
+            </form>
+            <div>
+                <button>Continue With Google</button>
+                <button>Continue With Github</button>
+            </div>
+        </div>
+    );
+};
+
+export default Auth;
